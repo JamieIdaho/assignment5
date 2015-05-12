655b45bb098209090e729dbc9ef4da0b7eb2a7d5
Jamie Gamblin <jamiemgamblin@gmail.com>
2015-05-12 16:16:06 -0400

    Added subtract, multiply, divide in HTML and added functions for them in JS

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-var calculate = document.querySelector("#calculateBtn");
+var addButton = document.querySelector("#addBtn");
+var subtractButton = document.querySelector("#subtractBtn");
+var multiplyButton = document.querySelector("#multiplyBtn");
+var divideButton = document.querySelector("#divideBtn");
+
 
 
 function add() {
@@ -10,5 +14,35 @@ function add() {
   document.querySelector("#answer").textContent = answer;
 }
 
+function subtract() {
+  var numOne = document.querySelector("#num_one").value;
+  var numTwo = document.querySelector("#num_two").value;
+  var chosenNum1 = Number(numOne);
+  var chosenNum2 = Number(numTwo);
+  var answer = chosenNum1 - chosenNum2;
+  document.querySelector("#answer").textContent = answer;
+}
 
-calculate.addEventListener("click", add);
+function multiply() {
+  var numOne = document.querySelector("#num_one").value;
+  var numTwo = document.querySelector("#num_two").value;
+  var chosenNum1 = Number(numOne);
+  var chosenNum2 = Number(numTwo);
+  var answer = chosenNum1 * chosenNum2;
+  document.querySelector("#answer").textContent = answer;
+}
+
+function divide() {
+  var numOne = document.querySelector("#num_one").value;
+  var numTwo = document.querySelector("#num_two").value;
+  var chosenNum1 = Number(numOne);
+  var chosenNum2 = Number(numTwo);
+  var answer = chosenNum1 / chosenNum2;
+  document.querySelector("#answer").textContent = answer;
+}
+
+addButton.addEventListener("click", add);
+subtractButton.addEventListener("click", subtract);
+multiplyButton.addEventListener("click", multiply);
+divideButton.addEventListener("click", divide);
+
